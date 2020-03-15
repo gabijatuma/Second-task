b3690a3a646803bf17d3338152b8bfb44a2214b7
gabijatuma <60934852+gabijatuma@users.noreply.github.com>
2020-03-15 23:40:06 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <time.h>
 #include <string>
 #include <chrono>
+#include <list>
+#include <deque>
 #include "tasks.h"
 
 using namespace std;
@@ -18,13 +20,20 @@ int main()
 
 
     char type = 'v';
+    int kiekis = 1000;
+    cout<<"iveskite studentu kieki. 1000, 10000, 100000, 1000000 arba 10000000"<<endl;
+    cin>>kiekis;
+    if(kiekis<1000 && kiekis > 10000000 && kiekis%10 !=0)
+        kiekis = 1000;
 
-    create(1000, type);
-    create(10000, type);
-    create(100000, type);
-    create(1000000, type);
-    create(10000000, type);
-
+    create(kiekis, type);
+    readfileV(type, kiekis);
+    readfileD(type, kiekis);
+    readfileL(type, kiekis);
 
     return 0;
 }
+
+
+
+
